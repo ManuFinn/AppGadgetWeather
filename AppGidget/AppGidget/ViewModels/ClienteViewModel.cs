66052b43498ac4aae0a2661bdce4d5456327aba2
ManuFinn <Manu_Finn117@outlook.com>
2021-12-06 16:11:19 -0600

    Agregados los Using en el VM

--- AppGidget/AppGidget/ViewModels/ClienteViewModel.cs
+++ AppGidget/AppGidget/ViewModels/ClienteViewModel.cs
@@ -1,6 +1,16 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Net.Http;
 using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
+using System.ComponentModel;
+using AppGidget.Models;
 
 namespace AppGidget.ViewModels
 {
