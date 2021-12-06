bcd6a63530e987d480a5ddd78a1b3c0ead36e856
Juan Jesús Chávez Villa <janitochvvll2311@gmail.com>
2021-12-06 17:19:56 -0600

    Notify en ViewModel

--- AppGidget/AppGidget/ViewModels/ClienteViewModel.cs
+++ AppGidget/AppGidget/ViewModels/ClienteViewModel.cs
@@ -11,34 +11,36 @@ using System.IO;
 using System.Threading;
 using System.ComponentModel;
 using AppGidget.Models;
+using System.Runtime.CompilerServices;
 
 namespace AppGidget.ViewModels
 {
-    public class ClienteViewModel
+    public class ClienteViewModel : INotifyPropertyChanged
     {
-        public string url { get; set; } = "http://192.168.1.72/info";
 
         public List<ClimaClase> Climas { get; set; } =
             new List<ClimaClase>();
 
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void NotifyPropertyChanged([CallerMemberName]string propertyname = null) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyname));
+        }
+
         // {"weather":{"main":"clouds","description":"broken clouds","temperature":18.19},
         // "datetime":{"date":"2021-12-06","time":"3:37 PM","isday":true}}
 
-        private string main, description;
-        private int temperature;
-        private DateTime date, time;
-        private bool isday;
+        private string url = "http://192.168.1.72/info";
+        private ClimaClase clima = new ClimaClase();
+        private string mensaje = "";
 
-        public string Mainx { get { return main; } set { main = value; } }
-        public string Descriptionx { get { return description; } set { description = value; } }
-        public int Temperaturex { get { return temperature; } set { temperature = value; } }
-        public DateTime Datex { get { return date; } set { date = value; } }
-        public DateTime Timex { get { return time; } set { time = value; } }
-        public bool IsDay { get { return isday; } set { isday = value; } }
+        public string Url { get => url; set { url = value; NotifyPropertyChanged(); } }
 
-        public string mensaje { get; set; }
+        public ClimaClase Clima {get => clima; set { clima = value; NotifyPropertyChanged(); }}
 
-        public ICommand ActualizarClima { get; set; }
+        public string Mensaje { get => mensaje; set { mensaje = value; NotifyPropertyChanged(); } }
+
+        public ICommand ActualizarClima { get; }
 
         public ClienteViewModel()
         {
@@ -49,5 +51,6 @@ namespace AppGidget.ViewModels
         {
             throw new NotImplementedException();
         }
+
     }
 }
